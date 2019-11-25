81706adb3ac42ebfbb86f73f64dbadd84afa2e1e
zFightx <49701831+zFightx@users.noreply.github.com>
2019-11-25 06:48:43 -1000

    New

--- electron_GUI/loja.js
+++ electron_GUI/loja.js
@@ -1,5 +1,5 @@
-function carregarInventario(){
-    var lista = document.getElementById("listaItens");
+function carregarLoja(){
+    var lista = document.getElementById("itensLoja");
 
     /* Ler Banco de Dados aqui */
     var itens = bancoDeDados();
@@ -9,40 +9,18 @@ function carregarInventario(){
         lista.innerHTML = anterior +
         "<li onclick=\"selecionarItem(this, '" + itens[i][1] + "')\">" +
             "<scan class=\"quantia\">" + itens[i][13] + "</scan>" +
-            "<scan class=\"preco\">" + itens[i][2] + "</scan>" +
+            "<scan class=\"preco\">" + itens[i][3] + "</scan>" +
             "<scan class=\"image\"><img src=\"_imagens/" + "MachadodeFogo" + ".png\"></scan>" +
             "<scan class=\"tipo\">" + itens[i][13] + "</scan>" +
+            "<scan class=\"sumir dano\">" + itens[i][4] + "</scan>" +
+            "<scan class=\"sumir forca\">" + itens[i][5] + "</scan>" +
+            "<scan class=\"sumir intelecto\">" + itens[i][6] + "</scan>" +
+            "<scan class=\"sumir vigor\">" + itens[i][7] + "</scan>" +
+            "<scan class=\"sumir armadura\">" + itens[i][8] + "</scan>" +
+            "<scan class=\"sumir durabilidade\">" + itens[i][9] + "</scan>" +
         "</li>";
         anterior = lista.innerHTML;
-    }
-
-    itens = bancoDeDados2();
-
-    lista = document.getElementById("inventario-esquerdo");
-    anterior = "";
-      
-    for (var i = 0; i < itens.length/2; i++){
-        lista.innerHTML = anterior +
-        "<li class=\"itensInv cabeca\" id=\"cabeca\" onclick=\"selecionarItem(this, '" + itens[i][1] + "')\">" +
-            "<scan class=\"image\"><img src=\"_imagens/EspadaCongelante.png\"></scan>" +
-        "</li>";
-
-        anterior = lista.innerHTML;
-        
-    }
-    
-    lista = document.getElementById("inventario-direito");
-    anterior = "";
-
-    for (var i = itens.length/2; i < itens.length; i++){
-        lista.innerHTML = anterior +
-        "<li class=\"itensInv cabeca\" id=\"cabeca\" onclick=\"selecionarItem(this, '" + itens[i][1] + "')\">" +
-            "<scan class=\"image\"><img src=\"_imagens/EspadaCongelante.png\"></scan>" +
-        "</li>";
-
-        anterior = lista.innerHTML;
-    }
-    
+    }    
 }
 
 function bancoDeDados(){
@@ -62,7 +40,9 @@ function bancoDeDados(){
     [2,"Espada Congelante", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15],
     [3,"Escudo de Ferro", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15],
     [4,"Vestido da Morte", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15],
-    [5,"Garra Sombria", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15]
+    [5,"Garra Sombria", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15],
+    [5,"Arara Sombria", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15],
+    [5,"Arara Sombria", 0, 1200, 10,10,10,10,10,"Ninja e Guerreiro", 20, 10, "FA",15]
     ];
 
     return result;
